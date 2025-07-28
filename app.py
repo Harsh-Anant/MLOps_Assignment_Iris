feec217b1954dcea6944b6ce38dd6ab9fba8012c
Harsh-Anant <harshanant8876@gmail.com>
2025-07-28 15:10:23 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -3,8 +3,14 @@ import mlflow.pyfunc
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
 import pandas as pd
+import os
+import sqlite3
+from src.logging_and_monitoring import init_db, log_prediction
+
 
 app = FastAPI()
+
+init_db()
 
 # Load the model from MLflow Model Registry
 
@@ -12,17 +18,16 @@ app = FastAPI()
 model = mlflow.pyfunc.load_model("models:/IrisSpeciesClassifier/1")  # or use 'Production'
 
 # Define input data schema using Pydantic
-#from pydantic import BaseModel, Field
 
 class IrisInput(BaseModel):
-    sepal_length_cm: float = Field(..., alias="sepal length (cm)")
-    sepal_width_cm: float = Field(..., alias="sepal width (cm)")
-    petal_length_cm: float = Field(..., alias="petal length (cm)")
-    petal_width_cm: float = Field(..., alias="petal width (cm)")
+    sepal_length_cm: float = Field(..., alias="sepal length (cm)",ge=0, le=10, description="Range: 0 to 10 cm")
+    sepal_width_cm: float = Field(..., alias="sepal width (cm)", ge=0, le=10, description="Range: 0 to 10 cm")
+    petal_length_cm: float = Field(..., alias="petal length (cm)", ge=0, le=10, description="Range: 0 to 10 cm")
+    petal_width_cm: float = Field(..., alias="petal width (cm)", ge=0, le=10, description="Range: 0 to 10 cm")
 
 class Config:
     allow_population_by_field_name = True  # Needed when using aliases
-
+    
 @app.get("/")
 def home():
     return {"message": "FastAPI is running!"}
@@ -38,11 +43,32 @@ def predict_species(data: IrisInput):
     }
     input_df = pd.DataFrame([input_dict])
     prediction = model.predict(input_df)
-    iris_category = ''
+    
+    prediction = prediction.tolist()[0]
+    
+    iris_category = 'Invalid Input!, make sure each of the four attributes are there and their values ranges from 0 to 10 in cm.'
+    
     if prediction == 0:
         iris_category = 'setosa'
     elif prediction == 1:
         iris_category = 'versicolor'
     elif prediction == 2:
         iris_category = 'virginica'
+        
+    input_to_log = {
+        "sepal_length_cm": data.sepal_length_cm,
+        "sepal_width_cm": data.sepal_width_cm,
+        "petal_length_cm": data.petal_length_cm,
+        "petal_width_cm": data.petal_width_cm
+    }  
+    log_prediction(input_to_log, [prediction,iris_category])
     return {"prediction": iris_category}
+
+@app.get("/logs")
+def get_logs():
+    conn = sqlite3.connect("predictions.db")
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM predictions ORDER BY timestamp desc")
+    rows = cursor.fetchall()
+    conn.close()
+    return {"logs": rows}
---
+++ src/logging_and_monitoring.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import sqlite3
+from datetime import datetime
+import json
+import os
+
+
+# In[4]:
+
+
+DB_PATH = "predictions.db"
+
+def init_db():
+    # Create DB only if it doesn't exist
+    if not os.path.exists(DB_PATH):
+        conn = sqlite3.connect(DB_PATH)
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS predictions (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                sepal_length REAL,
+                sepal_width REAL,
+                petal_length REAL,
+                petal_width REAL,
+                prediction INTEGER,
+                prediction_label TEXT,
+                timestamp TEXT
+            )
+        ''')
+        conn.commit()
+        conn.close()
+
+def log_prediction(input_data: dict, prediction: list):
+    conn = sqlite3.connect(DB_PATH)
+    cursor = conn.cursor()
+    cursor.execute(
+        '''
+        INSERT INTO predictions (timestamp, sepal_length, sepal_width, petal_length, petal_width, prediction,prediction_label)
+        VALUES (?, ?, ?, ?, ?, ?,?)
+        ''',
+        (
+            str(datetime.now()),
+            input_data["sepal_length_cm"],
+            input_data["sepal_width_cm"],
+            input_data["petal_length_cm"],
+            input_data["petal_width_cm"],
+            prediction[0],
+            prediction[1]
+        )
+    )
+    conn.commit()
+    conn.close()
+
+
+# In[ ]:
+
+
+
+
