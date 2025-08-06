f6de48b0db2114d8da91d22b0c16b200e1d348c8
Harsh-Anant <68021050+Harsh-Anant@users.noreply.github.com>
2025-08-06 15:43:05 +0530

    Update model_retraining_and_mlflow_registering.py

--- src/model_retraining_and_mlflow_registering.py
+++ src/model_retraining_and_mlflow_registering.py
@@ -1,13 +1,13 @@
 # retrain_pipeline.py
 
 import sqlite3
-import pandas as pd
-import joblib
-import mlflow
 import csv
+import pandas as pd
+#import joblib
+import mlflow
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score,precision_score,f1_score,recall_score
 import sys
 
 def run_retraining():
